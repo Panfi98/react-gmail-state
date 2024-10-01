6fb404465e0027b98cb70a5dc1684472187eca46
Daniil Panfilov <daniilpanfilov@Daniils-MacBook-Pro.local>
2024-10-01 10:38:06 +0200

    finished with core+ 1 extension

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,32 @@
 import Header from './components/Header'
 import initialEmails from './data/emails'
-
+import { useState, useMemo } from 'react'
 import './styles/App.css'
 
 function App() {
   // Use initialEmails for state
   console.log(initialEmails)
+  const [emails, setEmails] = useState(initialEmails)
+  const [box, setBox] = useState('inbox')
+  const [star, setStar] = useState(false)
+
+  function toggleRead(target) {
+    const newEmails = emails.map(email =>
+      email.id === target.id ?
+        { ...email, read: !email.read } :
+        email
+    )
+    setEmails(newEmails)
+  }
+
+  function toggleStarred(target) {
+    const newEmails = emails.map(email =>
+      email.id === target.id ?
+        { ...email, starred: !email.starred } :
+        email
+    )
+    setEmails(newEmails)
+  }
 
   return (
     <div className="app">
@@ -14,14 +35,15 @@ function App() {
         <ul className="inbox-list">
           <li
             className="item active"
-            // onClick={() => {}}
+
+
           >
             <span className="label">Inbox</span>
             <span className="count">?</span>
           </li>
           <li
             className="item"
-            // onClick={() => {}}
+          //todo
           >
             <span className="label">Starred</span>
             <span className="count">?</span>
@@ -33,12 +55,38 @@ function App() {
               id="hide-read"
               type="checkbox"
               checked={false}
-              // onChange={() => {}}
+            //todo
             />
           </li>
         </ul>
       </nav>
-      <main className="emails">{/* Render a list of emails here */}</main>
+      <main className="emails">{
+        <ul>
+          {emails.map(email => (
+            <li className={`email ${email.read ? 'read' : ''}`} onClick={() => toggleRead(email)} key={email.id}>
+              <div className="select">
+                <input
+                  className="select-checkbox"
+                  type="checkbox"
+                />
+              </div>
+              <div className="star">
+                <input
+                  className="star-checkbox"
+                  type="checkbox"
+                  checked={email.starred}
+                  onChange={() => toggleStarred(email)}
+                />
+              </div>
+              <div className="sender">{email.sender}</div>
+              <div className="title">{email.title}</div>
+            </li>
+          ))}
+          <li>
+
+          </li>
+        </ul>
+      }</main>
     </div>
   )
 }
